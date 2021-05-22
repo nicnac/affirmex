4d3edc710ab73142889f31cf404afc8aa3beb65f
Loc Nguyen <l@nocsi.com>
2021-05-22 14:26:50 -0700

    first commit

---
+++ lib/affirmex.ex
@@ -0,0 +1,18 @@
+defmodule Affirmex do
+  @moduledoc """
+  Documentation for `Affirmex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Affirmex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/affirmex_test.exs
@@ -0,0 +1,8 @@
+defmodule AffirmexTest do
+  use ExUnit.Case
+  doctest Affirmex
+
+  test "greets the world" do
+    assert Affirmex.hello() == :world
+  end
+end
